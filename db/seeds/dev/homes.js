99a434402869124f1e8c1382c289b967af939706
johnmboudreaux <jhnbdrx@gmail.com>
2017-12-12 11:44:56 -0700

    added framing to homeowner seed file

--- db/seeds/dev/homes.js
+++ db/seeds/dev/homes.js
@@ -1,9 +1,7 @@
-
+/*eslint-disable */
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
   return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
+    .then(() => {
       return knex('table_name').insert([
         {id: 1, colName: 'rowValue1'},
         {id: 2, colName: 'rowValue2'},
