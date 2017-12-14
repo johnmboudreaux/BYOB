2b40e10802e6af64e30e2a0d3784cf2e7def5e05
johnmboudreaux <jhnbdrx@gmail.com>
2017-12-14 13:54:29 -0700

    got rid of linting errors in server js and added routes.spec.test

---
+++ test/routes.spec.test.js
@@ -0,0 +1,11 @@
+/*eslint-disable */
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const server = require('../server');
+
+const environment = process.env.NODE_ENV || 'test';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
+chai.use(chaiHttp);
